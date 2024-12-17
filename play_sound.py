ad29099317cfd9db10a0aa19d18e128a440bbf15
Ridhwan khan <ridhwankhan03@gmail.com>
2024-12-17 15:57:05 +0600

    all functionality cross checked

--- play_sound.py
+++
@@ -1,5 +0,0 @@
-import simpleaudio as sa
-
-sound_wave = sa.WaveObject.from_wave_file(r"D:\BRAC university\CSE\CSE423\Lab1\First Program\CSE_423_PROJECT\beep.wav")
-play_obj = sound_wave.play()
-play_obj.wait_done()
